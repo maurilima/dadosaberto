0b8503e585d46e7d07f202d30e50afbfdce443e6
unknown <mauri.lima.roraima@gmail.com>
2021-09-09 13:05:14 -0400

    Add Guage

---
+++ src/components/Gauge/index.js
@@ -0,0 +1,20 @@
+
+import GaugeChart from 'react-gauge-chart'
+
+import React from 'react';
+
+// import { Container } from './styles';
+
+
+function Gauge(props) {
+  return <GaugeChart id="gauge-chart1"
+  nrOfLevels={30} 
+  colors={[ "#00FF80","#00361B","#00361B"]} 
+  arcWidth={0.3} 
+  percent={props.value}
+  arcPadding={0.01}
+
+  textColor={'#00361BE'}  />;
+}
+
+export default Gauge;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap"
 import CardVisao from "../../components/CardVisao";
+import Gauge from "../../components/Gauge";
 import RakingOrgaos from "../../components/RakingOrgaos";
 import StatusPedido from "../../components/StatusPedido";
 import './index.css'
@@ -30,12 +31,20 @@ function Home() {
             <Col xs={12} xl={6} xxl={6}>
 
               <Row id='row-cards'>
-                <Col id='col-card' xs={12} sm={7} md={7} lg={7} xxl={6} >
+                <Col id='col-card' xs={12} sm={6} md={6} lg={6} xxl={6} >
                   <CardVisao subtitle='1.000.325'>PEDIDOS RECEBIDOS</CardVisao>
                 </Col>
-                <Col id='col-card' xs={12} sm={5} md={5} lg={5} xxl={6} >
+                <Col id='col-card' xs={12} sm={6} md={6} lg={6} xxl={6} >
                   <CardVisao
                     subtitle='15,16 dias' texto='Tempo médio de resposta que o Orgão leva pra responder ' >TEMPO MÉDIO DE RESPOSTA</CardVisao>
+                </Col>
+              </Row>
+              <Row id='row-gauge'>
+                <Col xs={12} sm={6} md={6} lg={6} xxl={6} >
+                <Gauge value={.70} ></Gauge> 
+                </Col>
+                <Col xs={12} sm={6} md={6} lg={6} xxl={6}>
+                <Gauge value={.85}/>
                 </Col>
               </Row>
             </Col>
@@ -50,6 +59,9 @@ function Home() {
         <renderProgress />
 
       </div>
+      <Row>
+       
+      </Row>
 
     </React.Fragment>
 
